1375e227fe7896f6c97ecf919f0ceec5501ad402
salimdason <20041232@studenti.uniupo.it>
2023-03-04 03:03:57 +0100

    testing

---
+++ tests/test_wasabis3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `wasabis3` package."""
+
+
+import unittest
+
+from wasabis3 import wasabis3
+
+
+class TestWasabis3(unittest.TestCase):
+    """Tests for `wasabis3` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
